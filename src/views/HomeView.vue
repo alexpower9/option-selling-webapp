594549d9fdaa9bbb40faf133ad44f96bb3bc45f3
Alex Power <alexpower9@gmail.com>
2024-02-05 19:35:25 -0500

    Added the basics for the search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
 <div class="top-bar">
     <TopBar></TopBar>
+    <SearchBar></SearchBar>
 </div>
 </template>
 
 <script>
 import TopBar from '@/components/TopBar.vue'
+import SearchBar from '@/components/SearchBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    TopBar
+    TopBar, SearchBar
   }
 }
 
