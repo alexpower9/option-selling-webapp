b312c601bf1aab13ee3c2a2477683699b353e0e9
Alex Power <alexpower9@gmail.com>
2024-02-06 19:30:27 -0500

    Added dynamic routing, can now display the ticker on the StockPage

---
+++ src/views/StockPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>This is the page for {{ ticker }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'StockPage',
+  created() {
+    console.log('StockPage created') //delete this later
+  },
+  props:['ticker']
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #000;
+    font-size: 2em;
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
