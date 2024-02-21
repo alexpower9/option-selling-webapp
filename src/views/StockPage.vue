cdc94dc8f689108ee6b44543246d415142608a87
Alex Power <alexpower9@gmail.com>
2024-02-20 19:13:35 -0500

    Added the option table

--- src/views/StockPage.vue
+++ src/views/StockPage.vue
@@ -1,21 +1,29 @@
 <template>
   <TopBar></TopBar>
+  <IndexDisplay></IndexDisplay>
   <h1>This is the page for {{ ticker }}</h1>
-  <h1>This is the current price {{ price }}</h1>
+  <div class="chain-container">
+    <CallChain v-if="optionData" :optionsData="optionData"></CallChain>
+    <PutChain v-if="optionData" :optionsData="optionData"></PutChain>
+  </div>
 </template>
 
 <script>
 import TopBar from '@/components/TopBar.vue'
 import axios from 'axios';
+import CallChain from '@/components/CallChain.vue';
+import PutChain from '@/components/PutChain.vue';
+import IndexDisplay from '@/components/IndexDisplay.vue';
 
 export default {
   name: 'StockPage',
-  components: {TopBar},
+  components: {TopBar, CallChain, PutChain, IndexDisplay},
   props:['ticker'],
   data() {
     return {
       price: null,
       strikes: [],
+      optionData: null
     };
   },
   methods: {
@@ -34,7 +42,8 @@ export default {
         }
       })
       .then(response => {
-        console.log("This is our response data",response.data);
+        this.optionData = response.data;
+        //console.log(this.optionsData);
       })
       .catch(error => {
         console.log(error);
@@ -62,5 +71,11 @@ h1 {
     font-size: 2em;
     text-align: center;
     margin-top: 20px;
+}
+
+.chain-container {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 60px;
 }
 </style>
